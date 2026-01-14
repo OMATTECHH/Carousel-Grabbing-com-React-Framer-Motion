b3d05280b939112cf51f05f56a6a698500f81daf
Mateus Monteiro Ramos <devteus.officeti@gmail.com>
2026-01-13 21:11:18 -0300

    Enviando coisas

--- pegar/src/main.jsx
+++ pegar/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/Carousel-Grabbing-com-React-Framer-Motion'>
     <Routes>
       <Route path='/*' element={<Home/>}/>
     </Routes>
--- pegar/vite.config.js
+++ pegar/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Carousel-Grabbing-com-React-Framer-Motion",
   plugins: [
     react(),
     tailwindcss(),
